909ceb16e83820ea4e30223ef1baaf0e9e7e5bab
Yusheng Wu <wuyusheng1023@gmail.com>
2021-02-04 18:26:53 +0200

    layout of ParamsForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,30 +3,31 @@ import 'antd/dist/antd.css'
 
 import Row from 'antd/lib/row';
 import Col from 'antd/lib/col';
+import ParamForm from './components/ParamsForm'
 
 function App() {
   return (
     <div className="App">
       <Row>
-        <Col span={18} offset={3} >
+        <Col span={20} offset={2} >
           header
         </Col>
       </Row>
       <Row>
-        <Col span={18} offset={3}>
-          header
+        <Col span={20} offset={2}>
+          <ParamForm/>
         </Col>
       </Row>
       <Row>
-        <Col span={6} offset={3}>
+        <Col span={5} offset={2}>
           sider
         </Col>
-        <Col span={12} offset={0}>
+        <Col span={15} offset={0}>
           contene
         </Col>
       </Row>
       <Row>
-        <Col span={18} offset={3}>
+        <Col span={20} offset={2}>
           footer
         </Col>
       </Row>
---
+++ frontend/src/components/ParamsForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+// import {
+//   Form,
+//   Button,
+//   Radio,
+//   Select,
+//   DatePicker,
+// } from 'antd';
+import Form from 'antd/lib/form'
+import Button from 'antd/lib/button'
+import Radio from 'antd/lib/radio'
+import Select from 'antd/lib/select'
+import DatePicker from 'antd/lib/date-picker'
+
+const ParamForm = () => {
+  const [componentSize, setComponentSize] = useState('default');
+  const [formLayout] = useState('horizontal');
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  const buttonItemLayout =
+    formLayout === 'horizontal' ?
+      {
+        wrapperCol: {
+          span: 14,
+          offset: 4,
+        },
+      } :
+      null;
+  
+  return (
+    <>
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+        <Form.Item label="Station" name="station">
+          <Radio.Group>
+            <Radio.Button value="VAR">SMEAR I Värriö forest</Radio.Button>
+            <Radio.Button value="HYY">SMEAR II Hyytiälä forest</Radio.Button>
+            <Radio.Button value="KUM">SMEAR III Helsinki Kumpula</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item label="From">
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item label="To">
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item label="Processing Level">
+          <Select>
+            <Select.Option value="ANY">ANY</Select.Option>
+            <Select.Option value="CHECKED">CHECKED</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Averaging">
+          <Select>
+            <Select.Option value="1 min">1 min</Select.Option>
+            <Select.Option value="30 min">30 min</Select.Option>
+            <Select.Option value="1 hour">1 hour</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="Averaging Type">
+          <Select>
+            <Select.Option value="NONE">NONE</Select.Option>
+            <Select.Option value="ARITHMETIC">ARITHMETIC</Select.Option>
+            <Select.Option value="GEOMETRIC">GEOMETRIC</Select.Option>
+            <Select.Option value="SUN">SUN</Select.Option>
+            <Select.Option value="MEDIAN">MEDIAN</Select.Option>
+            <Select.Option value="MIN">MIN</Select.Option>
+            <Select.Option value="MAX">MAX</Select.Option>
+            <Select.Option value="CIRCULAR">CIRCULAR</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item {...buttonItemLayout}>
+          <Button type="primary">Submit</Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+
+export default ParamForm
