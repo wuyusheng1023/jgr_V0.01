6fa8d569f4f6c2c5273a8e98c9d60e068942037f
Yusheng Wu <wuyusheng1023@gmail.com>
2021-02-05 19:53:43 +0200

    move setting out from App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,13 @@ import ParamForm from './components/ParamsForm.jsx'
 import DateList from './components/DateList.jsx'
 import ChartsFrame from './components/ChartsFrame.jsx'
 
+import setting from './setting';
+const {rootEndpoint, dpList} = setting
+
 function App() {
   const [queryMeta, setQueryMeta] = useState({isSubmitted: false})
   const [dateList, setDateList] = useState()
   const [runID, setRunID] = useState(-1)
-
-  // const url = "https://smear-backend.rahtiapp.fi/search/timeseries"
-
-  // const dpList = [
-  //   "d100e1", "d112e1", "d126e1", "d141e1", "d158e1", "d178e1", "d200e1", "d224e1", "d251e1", "d282e1",
-  //   "d316e1", "d355e1", "d398e1", "d447e1", "d501e1", "d562e1", "d631e1", "d708e1", "d794e1", "d891e1",
-  //   "d100e2", "d112e2", "d126e2", "d141e2", "d158e2", "d178e2", "d200e2", "d224e2", "d251e2", "d282e2",
-  //   "d316e2", "d355e2", "d398e2", "d447e2", "d501e2", "d562e2", "d631e2", "d708e2", "d794e2", "d891e2",
-  //   "d100e3", "d112e3", "d126e3", "d141e3", "d158e3", "d178e3", "d200e3", "d224e3", "d251e3", "d282e3",
-  //   "d316e3", "d355e3", "d398e3", "d447e3", "d501e3", "d562e3", "d631e3", "d708e3", "d794e3", "d891e3",
-  //   "d100e4"]
 
   // from: dateList[0] + "T00:00:00.000",
   // to: dateList[1] + "T23:59:59.999"
